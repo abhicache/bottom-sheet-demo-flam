9970dce8033a6707cbc6281d684d5756198ab6f9
Abhishek Baranwal <baranwalabhishek21@gmail.com>
2025-07-09 15:29:13 +0530

    basic bottom sheet component static positions created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import BottomSheet from './components/BottomSheet'
 
 function App() {
   
   return (
     <>
       <div>
-        <h1>react Bottom sheet assignment</h1>
+        <h1>React Bottom sheet assignment</h1>
+        <p>Main content</p>
+        <BottomSheet>
+          <h2>Bottom Sheet Content</h2>
+          <p>inside the bottom sheet</p>
+          <p>More content here....</p>
+        </BottomSheet>
       </div>
     </>
   )
--- src/components/BottomSheet.jsx
+++ src/components/BottomSheet.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './BottomSheet.css' 
+const BottomSheet = ({children}) => {
 
-const BottomSheet = () => {
+    const [position, setPosition] = useState('bottom')
   return (
-    <div>BottomSheet</div>
+     <>
+      <div className="demo-controls">
+        <button onClick={() => setPosition('closed')}>Closed</button>
+        <button onClick={() => setPosition('half')}>Half</button>
+        <button onClick={() => setPosition('full')}>Full</button>
+      </div>
+      <div className={`bottom-sheet bottom-sheet--${position}`}>
+        <div className="bottom-sheet__handle"></div>
+        <div className="bottom-sheet__content">
+          {children}
+        </div>
+      </div>
+    </>
   )
 }
 
